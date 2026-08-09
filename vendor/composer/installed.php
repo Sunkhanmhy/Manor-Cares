eddd4ae824e02e2bc99ee5f550b415d1ffee671f
Shrautom Dev <shrautom@shrautom.com>
2026-08-09 12:42:31 +0100

    Push

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '37c32b247450a25583cecb1f6b7dc8567b5fc280',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '37c32b247450a25583cecb1f6b7dc8567b5fc280',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
